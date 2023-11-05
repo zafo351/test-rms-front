ce4a5471e0f6573c1369390a8509a8278b6c50a4
zafo351 <nicolacho2727@gmail.com>
2023-11-05 06:46:00 -0500

    ok

--- src/app/service/apiservice.service.ts
+++ src/app/service/apiservice.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class CalculadoraService {
-  private apiUrl = 'http://localhost:8081';
+  private apiUrl = 'http://localhost:8081/api/tasks';
 
   constructor(private http: HttpClient) {}
 
